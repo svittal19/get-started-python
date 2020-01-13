2780463f6d79a66bfe63320c0db4d34f286066b5
Shwetha Vittal <cs19resch01001@iith.ac.in>
2020-01-13 20:48:01 +0530

    Trying change in hello.py

--- hello.py
+++ hello.py
@@ -51,6 +51,7 @@ def root():
 @app.route('/api/visitors', methods=['GET'])
 def get_visitor():
     if client:
+        print('Going to pick visitor info from database')
         return jsonify(list(map(lambda doc: doc['name'], db)))
     else:
         print('No database')
